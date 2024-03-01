bff77cc2b967fc31c919fc7a56657b3cb3f197e4
Mohamed moustafa <104618250+mphamedm@users.noreply.github.com>
2024-03-02 00:11:04 +0200

    Add files via upload

---
+++ qran.js
@@ -0,0 +1,83 @@
+let live = document.getElementById("live");
+let test= document.getElementById("test");
+let uli = document.getElementById("uli")
+let resters= document.getElementById( "resters");
+
+
+
+
+
+function on() {
+  let uli =document.getElementById("uli");
+  if (uli.style.display==="none") {
+    uli.style.display="flex"
+    
+  } else {
+    uli.style.display="none"
+    
+  }
+  
+  
+  
+  
+
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+
+
+
+
+
+
+
+
+  
+  
+
+
+ 
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
